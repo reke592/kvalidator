dc33d839e7a0f7a831bc43d9862cf5bdc66fc517
erric rapsing <erric.rapsing@gmail.com>
2017-06-14 20:24:31 +0800

    skip validation if no rule

--- test.js
+++ test.js
@@ -4,15 +4,22 @@ const Validator = require('./lib/Validator')
 	min:30
 */
 const data = {
-	age: '20',
-	name: 'reke'
+	age: '16',
+	name: 'reke',
+  message: 'Tinky-winky, Dipsy, Laa-Laa, Po, Po. Tinky-winky, Dipsy, Laa-Laa, Po, Po.',
+  
+  no_rules_for_address: 'Norzagaray, Bulacan. Philippines'
 }
+
 
 const val = Validator({
 	name: 'string|min:2|max:5|required',
-	age: 'number|min:18|max:30|required'
+	age: 'number|min:18|max:30|required',
+  message: 'string|min:10|max:20',
+  
+  useless_rule: 'number|min:5|max:100'
 })
 
-let result = val.validate(data)
 
+let result = val.validate(data)
 console.log(result)
