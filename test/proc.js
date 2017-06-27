a95b2884a24c49d1b819f5723c90af74a4c367e0
reke592 <erric.rapsing@gmail.com>
2017-06-27 16:24:35 +0800

    ongoing: create background process

---
+++ test/proc.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+// const ws = fs.createWriteStream('./data.txt')
+
+// const d = require('./data.txt')
+
+// console.log(d[0])
+
+console.log(path.resolve('./data.txt'))
+
+// let items = 1000
+// ws.write('[')
+// for(var i = 0; i<items; i++)
+// 	ws.write(JSON.stringify({
+// 		id: i,
+// 		name: 'reke',
+// 		age: 25
+// 	})  + ',\n')
+// ws.write(']')
